c57f87a079920b19db327ee94163b73092b4d893
Koser61 <kosinskipiotr.98@gmail.com>
2022-04-20 16:27:57 +0200

    Add ActiveFilter functionality

--- src/components/ActiveFilter/ActiveFilter.js
+++ src/components/ActiveFilter/ActiveFilter.js
@@ -4,17 +4,45 @@ import { ReactComponent as RemoveIcon } from '../../images/icon-remove.svg';
 
 import styles from './ActiveFilter.module.scss';
 
-const ActiveFilter = ({ value }) => (
-  <div className={styles.component}>
-    <p>{value}</p>
-    <button className={styles.removeButton}>
-      <RemoveIcon />
-    </button>
-  </div>
-);
+const ActiveFilter = ({ property, value, dispatch }) => {
+  const removeFilter = () => {
+    const getActionObject = () => {
+      switch(property) {
+        case 'role':
+          return { type: 'SET_ROLE_FILTER', value: '' };
+        case 'level':
+          return { type: 'SET_LEVEL_FILTER', value: '' };
+        case 'languages':
+          return { type: 'REMOVE_LANGUAGE_FILTER', value: value };
+        case 'tools':
+          return { type: 'REMOVE_TOOLS_FILTER', value: value };
+        default:
+          return;
+      }
+    };
+
+    const actionObject = getActionObject();
+
+    dispatch(actionObject);
+  };
+
+  return (
+    <div className={styles.component}>
+      <p>{value}</p>
+      <button
+        className={styles.removeButton}
+        onClick={() => removeFilter()}
+      >
+        <RemoveIcon />
+      </button>
+    </div>
+  );
+};
 
 ActiveFilter.propTypes = {
+  property: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 export default ActiveFilter;
